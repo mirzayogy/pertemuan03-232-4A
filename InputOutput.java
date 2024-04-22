82cbcd9182fecf82291e25fbfe5ac1ec931783c6
mirzayogy <mirza.yogy@gmail.com>
2024-04-22 11:49:05 +0800

    input nama

--- InputOutput.java
+++ InputOutput.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class InputOutput{
     public static void main(String[] args){
+        int nilai = 0;
+        double rata = 0;
+        String nama = "Mirza";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nama :" + nama);
+        System.out.println("Input nama baru: ");
+        nama = scanner.nextLine();
+        System.out.println("Nama baru:" + nama);
         
     }
+
 }
